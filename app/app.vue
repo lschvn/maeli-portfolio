7611cc4bec20854766c5e73d71fd5b414ac586b1
LouisLSCHVN <louis.leschevin@gmail.com>
2025-02-26 16:17:01 +0100

    init: enter animation test

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtLoadingIndicator/>
+    <AnimationEnter />
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  routeRules: {
+    '/': { static: true }
+  },
+
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/scripts'],
+  css: ['~/assets/css/main.css']
+})
