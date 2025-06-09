f1adf985a483615918f9cf32e15a168c5577c0df
lschvn <louis.leschevin@gmail.com>
2025-06-09 20:05:54 +0000

    fix: update project tags and descriptions; add new images for Horscape

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -2,28 +2,28 @@ export const PROJECTS = {
   "marques": [
     {
       "name": "CHRIZ",
-      "tags": ["print", "branding"],
+      "tags": ["bière locale"],
       "path": "chriz",
       "description": "Dans le cadre d’un projet fictif, je devais imaginer <br>une marque de <strong>bière originaire de Cherbourg</strong>. <br>J’ai alors créé CHIRZ, une marque à l’esthétique <strong>maritime</strong>, <br>en hommage à la ville bord de mer. Son nom est aussi <br>un clin d’œil à la manière de trinquer en anglais, \"cheers\"."
-      ,"img": 3
+      ,"img": 2
     },
     {
       "name": "FRUIZZZ",
-      "tags": ["packaging", "concept"],
+      "tags": ["boisson énergisante", "concept"],
       "path": "fruizzz",
       "description": "Dans le cadre d'un projet fictif, notre objectif était de créer <br>une marque de <strong>boisson énergisante</strong>. J’ai ainsi imaginé Fruizzz, <br>avec le citron comme élément principal, et des couleurs jaunes <br>et vertes pour évoquer <strong>l’énergie et la fraîcheur</strong>."
-      ,"img": 3
+      ,"img": 2
     },
     {
       "name": "HORSCAPE",
-      "tags": ["3D", "digital"],
+      "tags": ["site", "location de box"],
       "path": "horscape",
       "description": "J’ai travaillé pour <strong>Horsplace</strong>, une entreprise innovante dans la gestion <br>des infrastructures équestres. Elle proposait d’abord des tablettes robustes <br>pour les écuries, puis a élargi son offre avec une plateforme de <strong>réservation de box</strong> <br>pour chevaux. J’ai réalisé la maquette du site ainsi que tout le <strong>rebranding</strong>, <br>en renommant la plateforme  \"Horscape\", à partir du préfixe souhaité par le client : \"Hors\"."
     
-    ,"img": 3},
+    ,"img": 5},
     {
       "name": "MAYA",
-      "tags": ["3D", "digital"],
+      "tags": ["da", "crème solaire"],
       "path": "maya",
       "description": "Dans le cadre d’un workshop, nous avons travaillé à partir du nom <br>de marque imposé « Maya ». À partir de cela, chacun devait imaginer <br>un produit ou un service. J’ai choisi de développer une marque de crème <br>solaire bio, naturelle, non polluante pour l’océan, avec la noix de coco <br>comme ingrédient principal."
       ,"img": 1
@@ -32,14 +32,14 @@ export const PROJECTS = {
   "creations": [
     {
       "name": "BEAUREGARD",
-      "tags": ["design", "branding"],
+      "tags": ["déclinaisons", "festival beauregard"],
       "path": "beauregard",
       "description": "Ma mission était d'imaginer l'identité visuelle que le festival <strong>Beauregard</strong> <br>aurait pu adopter pour l’édition 2024-2025. J’ai ensuite réalisé différentes déclinaisons <br>de cette direction artistique."
       ,"img": 2
     },
     {
       "name": "CYBER-RACING",
-      "tags": ["digital", "gaming"],
+      "tags": ["jeux de plateau", "éducatif"],
       "path": "cyber-racing",
       "description": "Dans le cadre d’un partiel, j’ai conçu un <strong>jeu éducatif</strong> en anglais en choisissant <br>parmi plusieurs thèmes imposés. J’ai opté pour la <strong>cybersécurité</strong> et développé <br>une direction artistique complète afin de le rendre à la fois ludique et accessible."
       ,"img": 3
@@ -74,21 +74,21 @@ export const PROJECTS = {
   "maquettes": [
     {
       "name": "PORTFOLIO LOUIS LSCHVN",
-      "tags": ["design", "UI"],
+      "tags": ["da", "site"],
       "path": "portfolio-louis-lschvn",
       "description": "J’ai réalisé la maquette et la <strong>direction artistique</strong> du portfolio <br>du <strong>développeur web</strong> Louis Leschevin. Son site est disponible ici : <br>https://louis-leschevin.com."
       ,"img": 2
     },
     {
       "name": "RADTRIP",
-      "tags": ["travel", "design"],
+      "tags": ["radio aléatoire", "application"],
       "path": "radtrip",
       "description": "J’ai travaillé sur la conception d’une application de <strong>radio</strong><br> dont le principe repose sur une écoute entièrement <strong>aléatoire</strong><br>. L’objectif était de permettre aux utilisateurs de découvrir <br>des musiques du monde de manière spontanée et immersive."
       ,"img": 3
     },
     {
       "name": "ASSO VOVINAM CAEN",
-      "tags": ["design", "UI"],
+      "tags": ["art martial", "site"],
       "path": "asso-vovinam-caen",
       "description": "J’ai réalisé la maquette fictive du site web d’une association de <br>Vovinam à Caen, qui n’en possédait pas encore. Je leur ai donc proposé <br>une version en accord avec leur ton et leur direction artistique, en <br>respectant l’univers des arts martiaux."
       ,"img": 2
